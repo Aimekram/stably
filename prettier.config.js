2c2f440d7af5983833b3919190ca13af0975860d
Nina <janina.byszewska@gmail.com>
2025-01-25 19:56:39 +0100

    chore: adjust prettier settings

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   printWidth: 100,
   tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  bracketSpacing: true,
   singleQuote: true,
   bracketSameLine: true,
   trailingComma: 'es5',
+  htmlWhitespaceSensitivity: 'strict',
 
   plugins: [require.resolve('prettier-plugin-tailwindcss')],
   tailwindAttributes: ['className'],
